5e26c4d9675851c77bc68637351ae7cfa62a47dd
Christopher Milligan <milliganjchristopher@gmail.com>
2019-01-30 15:29:10 +0000

    images added

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -7,15 +7,15 @@ class GameContainer extends Component {
     super(props);
     this.state = {
       squareStates: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9"
       ],
       player: "crosses",
       winner: "Start Game"
@@ -39,7 +39,7 @@ class GameContainer extends Component {
         else if (prevState.player === "naughts") {
           newPlayer = "crosses"
         }
-        const newWinner = this.checkWinner()+" wins";
+        const newWinner = this.checkWinner();
         return {squareStates: newState, player: newPlayer, winner: newWinner}
       }
     )
@@ -49,30 +49,33 @@ class GameContainer extends Component {
     const currentGrid = this.state.squareStates;
     //ROWS
     if (currentGrid[0] === currentGrid[1] && currentGrid[1] === currentGrid[2]) {
-      return currentGrid[1]
+      return currentGrid[1] + " wins!"
     }
     else if (currentGrid[3] === currentGrid[4] && currentGrid[4] === currentGrid[5]) {
-      return currentGrid[4]
+      return currentGrid[4] + " wins!"
     }
     else if (currentGrid[6] === currentGrid[7] && currentGrid[7] === currentGrid[8]) {
-      return currentGrid[7]
+      return currentGrid[7] + " wins!"
     }
     //COLUMNS
     else if (currentGrid[0] === currentGrid[3] && currentGrid[3] === currentGrid[6]) {
-      return currentGrid[0]
+      return currentGrid[0] + " wins!"
     }
     else if (currentGrid[1] === currentGrid[4] && currentGrid[4] === currentGrid[7]) {
-      return currentGrid[1]
+      return currentGrid[1] + " wins!"
     }
     else if (currentGrid[2] === currentGrid[5] && currentGrid[5] === currentGrid[8]) {
-      return currentGrid[2]
+      return currentGrid[2] + " wins!"
     }
     //DIAGONALS
     else if (currentGrid[0] === currentGrid[4] && currentGrid[4] === currentGrid[8]) {
-      return currentGrid[0]
+      return currentGrid[0] + " wins!"
     }
     else if (currentGrid[2] === currentGrid[4] && currentGrid[4] === currentGrid[6]) {
-      return currentGrid[2]
+      return currentGrid[2] + " wins!"
+    }
+    else {
+      return "Naughts and Crosses"
     }
   }
 
@@ -80,6 +83,7 @@ class GameContainer extends Component {
     return(
       <div className="App">
       <GameHeader winner={this.state.winner}/>
+      <br></br>
       <GridContainer handleGridSquareClicked={this.handleGridSquareClicked} squareStates={this.state.squareStates} player={this.state.player}/>
       </div>
     );
